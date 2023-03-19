266ebc743b9ba511f355f093f17b2d0b26b9e968
GitWAMH <61278893+GitWAMH@users.noreply.github.com>
2023-03-19 15:10:36 +0100

    Templates de las páginas
    Index y Menu

--- mysite/imageupload/urls.py
+++ mysite/imageupload/urls.py
@@ -6,5 +6,9 @@ from . import views
 app_name = 'imageupload'
 urlpatterns = [
     path('', views.ImageUploadView.as_view(), name='upload'),
+    path('index', views.index, name='index'),
+    path('menu/', views.menu, name='menu'),
+    path('menu/historial', views.historial, name='historial'),
+    path('menu/info', views.info, name='info'),
     path('webcam/', webcam_view, name='webcam'),
 ]
