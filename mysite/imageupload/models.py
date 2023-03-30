1b3265307c463379b694d9c6e27690686acdfa82
Dbugoi <60663710+Dbugoi@users.noreply.github.com>
2023-03-30 13:48:33 +0200

    Incluir el codigo de Rekognition y templates
    Incluir el codigo del rekognition en models y views con sus respectivos templates.
    Aun no funciona debido al formato de imagen. El resto si
    
    Co-Authored-By: dahermar <60484187+dahermar@users.noreply.github.com>
    Co-Authored-By: GitWAMH <61278893+GitWAMH@users.noreply.github.com>

--- mysite/imageupload/models.py
+++ mysite/imageupload/models.py
@@ -1,4 +1,89 @@
+import logging
+import boto3
+from botocore.exceptions import ClientError
 from django.db import models
+from rekognition_objects import (
+    RekognitionFace, RekognitionCelebrity, RekognitionLabel,
+    RekognitionModerationLabel, RekognitionText, show_bounding_boxes, show_polygons)
+from googletrans import Translator
+
+logger = logging.getLogger(__name__)
+
+
+class RekognitionImage:
+
+    def __init__(self, image, image_name, rekognition_client):
+        self.image = image
+        self.image_name = image_name
+        self.rekognition_client = rekognition_client
+
+    @classmethod
+    def from_file(cls, image_file_name, rekognition_client, image_name=None):
+        with open(image_file_name, 'rb') as img_file:
+            image = {'Bytes': img_file.read()}
+        name = image_file_name if image_name is None else image_name
+        return cls(image, name, rekognition_client)
+
+    @classmethod
+    def from_bucket(cls, s3_object, rekognition_client):
+        image = {'S3Object': {'Bucket': s3_object.bucket_name, 'Name': s3_object.key}}
+        return cls(image, s3_object.key, rekognition_client)
+
+    def detect_labels(self, max_labels):
+        try:
+            response = self.rekognition_client.detect_labels(
+                Image=self.image, MaxLabels=max_labels)
+            labels = [RekognitionLabel(label) for label in response['Labels']]
+            logger.info("Found %s labels in %s.", len(labels), self.image_name)
+        except ClientError:
+            logger.info("Couldn't detect labels in %s.", self.image_name)
+            raise
+        else:
+            return labels
+
+
+class Animal(models.Model):
+    name = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.name
+
 
 class Image(models.Model):
-    imagen = models.ImageField(upload_to='images/')
+    
+
+    image = models.ImageField(upload_to='images')
+    #detected_animal = models.ForeignKey(Animal, null=True, blank=True, on_delete=models.SET_NULL)
+    translation = models.CharField(max_length=255, null=True, blank=True)
+   
+    def __init__(self, image):
+            self.image = image
+    
+    def detect_animal(self):
+        # Initialize the Rekognition client
+        rekognition_client = boto3.client('rekognition')
+        # Detect labels in the image
+        image = RekognitionImage(image=self.image.read(), image_name=self.image.name, rekognition_client=rekognition_client)
+        labels = image.detect_labels(20)
+        # Look for the detected animal label
+        set_animals = set(Animal.objects.all().values_list('name', flat=True))
+        detected_animal = None
+        for label in labels:
+            if label.name in set_animals:
+                detected_animal = label.name
+                break
+        # Translate the animal name if detected
+        if detected_animal:
+            translator = Translator()
+            translation = translator.translate(detected_animal, dest='es').text
+        else:
+            translation = None
+        #return detected_animal, translation
+        return translation
+
+    def save(self, *args, **kwargs):
+        # Detect the animal and save the detection results before saving the image
+        detected_animal, translation = self.detect_animal()
+        self.detected_animal = Animal.objects.filter(name=detected_animal).first() if detected_animal else None
+        self.translation = translation
+        super
--- mysite/imageupload/views.py
+++ mysite/imageupload/views.py
@@ -1,7 +1,54 @@
 import os
 from django.conf import settings
+from django.views.generic.edit import CreateView
+from django.urls import reverse_lazy
+from .models import Image
+from django.shortcuts import render
 from django.shortcuts import render
 from .forms import ImageUploadForm
+from django.conf import settings
+import logging
+import boto3
+from botocore.exceptions import ClientError
+from pprint import pprint
+from googletrans import Translator
+from rekognition_objects import (
+    RekognitionFace, RekognitionCelebrity, RekognitionLabel,
+    RekognitionModerationLabel, RekognitionText, show_bounding_boxes, show_polygons)
+from .models import RekognitionImage
+
+logger = logging.getLogger(__name__)
+
+def rekognition_view(request):
+    if request.method == 'POST':
+        image_file = request.FILES.get('image')
+        print(image_file)
+        #rekognition_client = boto3.client('rekognition')
+
+        try:
+            image = {'Bytes': image_file.read()}
+            #print(image_file)
+            #print(image)
+            #image_name = image_file.name
+            #rekognition_image = RekognitionImage(image, image_name, rekognition_client)
+            #max_labels = 20
+            #labels = rekognition_image.detect_labels(max_labels)
+            imagen =Image(image=image)
+            animal = imagen.detect_animal()
+            #animal = type(image_file)
+            
+            #animal_es ="jirafa"
+            # Aquí se puede incluir cualquier código adicional para procesar los resultados obtenidos
+
+            # Convertir los resultados obtenidos en un diccionario para pasarlo a la plantilla
+            results ={"animal": animal}
+            return render(request, 'imageupload/rekognition_results.html', results)
+        except Exception as e:
+            logger.error(str(e))
+            error = 'Error: No se pudo procesar la imagen'
+           # return render(request, 'rekognition_error.html', {'error': error})
+
+    return render(request, 'imageupload/rekognition_form.html')
 
 
 def index(request):
@@ -10,7 +57,6 @@ def index(request):
         'latest_question_list': latest_question_list,
     }
     return render(request, 'imageupload/index.html', context)
-
 
 def menu(request):
     form = ImageUploadForm()
@@ -21,14 +67,16 @@ def menu(request):
 def historial(request):
     return render(request, 'imageupload/historial.html')
 
-
 def info(request):
-    return render(request, 'imageupload/info.html')
-
+    return render(request,'imageupload/info.html')
 
 def webcam_view(request):
     return render(request, 'imageupload/webcam.html')
 
+class ImageUploadView(CreateView):
+    model = Image
+    fields = ['title', 'image']
+    success_url = reverse_lazy('imageupload:index')
 
 def image_upload(request):
     if request.method == 'POST':
@@ -50,4 +98,4 @@ def image_upload(request):
             return render(request, 'imageupload/info.html', {'image_path': file_path})
     else:
         form = ImageUploadForm()
-    return render(request, 'imageupload/menu.html', {'form': form})
+    return render(request, 'imageupload/menu.html', {'form': form})    
